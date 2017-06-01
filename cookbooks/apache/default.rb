aca24a555d693c027ff24365008272d8074f90c1
tikasan <satak47cpc@gmail.com>
2017-06-01 23:19:50 +0900

    first commit

---
+++ cookbooks/apache/default.rb
@@ -0,0 +1,17 @@
+# Apacheインストール
+package 'httpd'
+
+# httpd.confのバックアップ
+execute 'httpd.conf backup' do
+    command 'cp /etc/httpd/conf/httpd.conf /etc/httpd/conf/httpd.conf.org'
+end
+
+# wwwユーザ追加
+execute 'add www user' do
+    user "root"
+    command <<-EOL
+        useradd www
+        gpasswd -a apache www
+        chown -R www:apache /var/www/html
+    EOL
+end
